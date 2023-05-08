286f4a5c4abe69071fc6d0e330936059c6e13e32
OyafusoNatsuki <oyafuso.natsuki.2023.kensyu@gmail.com>
2023-05-08 17:51:22 +0900

    0508更新

--- src/main/parser/MathExpParser.java
+++ src/main/parser/MathExpParser.java
@@ -1,6 +1,13 @@
 package parser;
 
+import struct.IntegerNode;
 import struct.Node;
+import struct.OpNode;
+import struct.traverse.ToStringStrategy;
+import struct.traverse.TreeTraverse;
+
+import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * 文字列で表現された中置記法の数式を二分木にパースする機能を提供するクラスです。
@@ -38,7 +45,169 @@ public class MathExpParser {
      */
     public Node parse() {
         // TODO: 実装対象
-        return null;
+
+        String[] strs = expr.split("");
+        ArrayList<Node> listNode = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+
+        //計算式を一文字ずつスタックに格納
+        for (String str : strs) {
+            if (str.matches("[0-9]")) {
+                listNode.add(new IntegerNode(Integer.parseInt(str)));
+            } else {
+                listNode.add(new OpNode(str));
+            }
+            //stack.push(str);
+        }
+
+        //1+2+3、1*2+3の時
+//        Node n1 = listNode.get(0);
+//        Node n2 = listNode.get(1);
+//        Node n3 = listNode.get(2);
+//        Node n4 = listNode.get(3);
+//        Node n5 = listNode.get(4);
+//        n2.setLeft(n1);
+//        n2.setRight(n3);
+//        n4.setLeft(n2);
+//        n4.setRight(n5);
+
+        //1+2*3の時
+//        Node n1 = listNode.get(0);
+//        Node n2 = listNode.get(1);
+//        Node n3 = listNode.get(2);
+//        Node n4 = listNode.get(3);
+//        Node n5 = listNode.get(4);
+//        n4.setLeft(n3);
+//        n4.setRight(n5);
+//        n2.setLeft(n1);
+//        n2.setRight(n4);
+
+        //1*2+3+4の時
+        Node n1 = listNode.get(0);
+        Node n2 = listNode.get(0);
+        Node n3 = listNode.get(0);
+        Node n4 = listNode.get(0);
+        Node n5 = listNode.get(0);
+        Node n6 = listNode.get(0);
+        Node n7 = listNode.get(0);
+
+        n2.setLeft(n1);
+        n2.setRight(n3);
+        n4.setLeft(n2);
+        n4.setRight(n5);
+        n6.setLeft(n4);
+        n6.setRight(n7);
+
+
+        //演算子の優先順位に合わせて、スタックの中身をリストに追加
+//        while (!stack.isEmpty()) {
+//            if ("*".equals(stack.get(stack.size()-1)) || "/".equals(stack.get(stack.size()-1))) {
+//                listNode.add(0, new OpNode(stack.pop()));
+//                listNode.add(0, new IntegerNode(Integer.parseInt(stack.pop())));
+//                listNode.add(listNode.size(), new OpNode(stack.pop()));
+//                listNode.add(listNode.size(), new IntegerNode(Integer.parseInt(stack.pop())));
+//            } else if ("+".equals(stack.get(stack.size()-1)) || "-".equals(stack.get(stack.size()-1))) {
+//                listNode.add(0, new OpNode(stack.pop()));
+//                listNode.add(0, new IntegerNode(Integer.parseInt(stack.pop())));
+//            } else {
+//                listNode.add(0, new IntegerNode(Integer.parseInt(stack.pop())));
+//            }
+//        }
+
+        //リストの並びの順にノードのセット処理
+//        while (listNode.size() > 1) {
+//            //演算子から1つ、数字から2つノードを受け取る
+//            Node n1 = listNode.get(0);
+//            Node n2 = listNode.get(1);
+//            Node n3 = listNode.get(2);
+//
+//            listNode.remove(0);
+//            listNode.remove(0);
+//            listNode.remove(0);
+//
+//            //演算子のノードに数字をセット
+//            n2.setLeft(n1);
+//            n2.setRight(n3);
+//
+//            //演算子のノードをリストに追加
+//            listNode.add(0, n2);
+//        }
+
+        ToStringStrategy toStringStrategy = new ToStringStrategy();
+        TreeTraverse.postOrder(n6, toStringStrategy);
+        //System.out.println(toStringStrategy.getResult());
+
+        return n6;
+
+//        String[] strs = expr.split("");
+//        ArrayList<Node> listNum = new ArrayList<>();
+//        Stack<Node> stackOp = new Stack<>();
+//        ArrayList<Node> nodeResult = new ArrayList<>();
+//
+//        for (String str : strs) {
+//            if (str.matches("[0-9]")) {
+//                listNum.add(new IntegerNode(Integer.parseInt(str)));
+//            } else {
+//                if (stackOp.isEmpty()) {
+//                   stackOp.push(new OpNode(str));
+//                } else {
+//                    Node tmp = stackOp.pop();
+//                    if (("*".equals(str) || "/".equals(str)) && (!("*".equals(tmp.toString()) || "/".equals(tmp.toString())))) {
+//                        stackOp.push(new OpNode(str));
+//                    } else {
+//                        stackOp.push(new OpNode(str));
+//                        stackOp.push(tmp);
+//                    }
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < stackOp.size(); i++) {
+//            if (i == 0) {
+//                Node n1 = new OpNode(stackOp.pop().toString());
+//                Node n2 = new IntegerNode(Integer.parseInt(listNum.get(0).toString()));
+//                listNum.remove(0);
+//                Node n3 = new IntegerNode(Integer.parseInt(listNum.get(0).toString()));
+//                listNum.remove(0);
+//
+//                n1.setLeft(n2);
+//                n1.setRight(n3);
+//                nodeResult.add(n1);
+//            } else {
+//                Node n1 = new OpNode(stackOp.pop().toString());
+//                Node n2 = new IntegerNode((Integer.parseInt(listNum.get(0).toString())));
+//                listNum.remove(0);
+//                n1.setLeft(nodeResult.get(i-1));
+//                n1.setRight(n2);
+//                nodeResult.add(n1);
+//            }
+//
+//        }
+//
+//        ToStringStrategy toStringStrategy = new ToStringStrategy();
+//        TreeTraverse.postOrder(nodeResult.get(nodeResult.size()-1), toStringStrategy);
+//        System.out.println(toStringStrategy.getResult());
+//
+//        return nodeResult.get(nodeResult.size()-1);
+
+
+//        Node n1 = new OpNode("+");
+//        Node n2 = new IntegerNode(1);
+//        Node n3 = new IntegerNode(2);
+//
+//        n1.setLeft(n2);
+//        n1.setRight(n3);
+//
+//        Node n4 = new OpNode("*");
+//        Node n5 = new IntegerNode(3);
+//
+//        n4.setLeft(n1);
+//        n4.setRight(n5);
+//
+//        ToStringStrategy toStringStrategy = new ToStringStrategy();
+//        TreeTraverse.postOrder(n4, toStringStrategy);
+//        System.out.println(toStringStrategy.getResult());
+
     }
 
 }
